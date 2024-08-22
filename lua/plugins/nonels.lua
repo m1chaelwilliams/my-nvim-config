639730393891ab42fdd2dbc8126b65fdd0333ffb
MICHAEL WILLIAMS <MICHAELWILLIAMSPERSONAL@GMAIL.COM>
2024-08-22 09:09:18 -0500

    added flash.nvim

--- lua/plugins/nonels.lua
+++ lua/plugins/nonels.lua
@@ -1,37 +1,37 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-		null_ls.setup({
-			autostart = true,
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.gofumpt,
-				null_ls.builtins.formatting.golines,
-				null_ls.builtins.formatting.goimports_reviser,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.clang_format,
-				null_ls.builtins.formatting.yamlfmt,
-			},
-			on_attach = function(client, buffer)
-				if client.supports_method("textDocument/formatting") then
-					vim.api.nvim_clear_autocmds({
-						group = augroup,
-						buffer = buffer,
-					})
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = augroup,
-						buffer = buffer,
-						callback = function()
-							vim.lsp.buf.format({ bufnr = buffer })
-						end,
-					})
-				end
-			end,
-		})
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+    null_ls.setup({
+      autostart = true,
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.gofumpt,
+        null_ls.builtins.formatting.golines,
+        null_ls.builtins.formatting.goimports_reviser,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.prettierd,
+        null_ls.builtins.formatting.clang_format,
+        null_ls.builtins.formatting.yamlfmt,
+      },
+      on_attach = function(client, buffer)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({
+            group = augroup,
+            buffer = buffer,
+          })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = buffer,
+            callback = function()
+              vim.lsp.buf.format({ bufnr = buffer })
+            end,
+          })
+        end
+      end,
+    })
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
